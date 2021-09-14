6f5c0a06cd15b059e261fa08700992fc7ff00ab1
Kamlesh <kamlesh_fw11_179@masai.school>
2021-09-14 17:54:08 +0530

    News App

---
+++ components/navBar.js
@@ -0,0 +1,7 @@
+function navB(){
+    return`<h1>News</h1>
+           <a href="home.html">Home</a>
+           <a href="news.html">News</a>
+           <a href="search.html">Search</a>`
+};
+export default navB;
